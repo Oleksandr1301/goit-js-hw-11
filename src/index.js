2461c2ecd8122b2880d70477cdd438a954abb603
Oleksandr1301 <viss82@ukr.net>
2023-02-15 16:35:28 +0200

    add simplelightbox

--- src/index.js
+++ src/index.js
@@ -1,5 +1,9 @@
 import Notiflix from 'notiflix';
 
+import SimpleLightbox from "simplelightbox";
+
+import "simplelightbox/dist/simple-lightbox.min.css";
+
 let gallery = new SimpleLightbox('.gallery', {
     captionDelay: 250,
     captionsData: "alt"
